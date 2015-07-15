17883669c871a87c0ee0023e5d6f6950b396542c
Kevin Solorio <kevin@detone8.com>
2015-07-15 15:37:53 -0700

    update attribute tests

--- part-1/spec/road_trip_spec.rb
+++ part-1/spec/road_trip_spec.rb
@@ -1,44 +1,52 @@
-require_relative '../road_trip'
-require_relative '../segment'
+require_relative '../shipping_container'
+require_relative '../crate'
 
-RSpec.describe RoadTrip do
-  let(:segments) { [Segment.new(miles: 500), Segment.new(miles: 750)] }
-  let(:road_trip) { RoadTrip.new(name: 'Trip to SF', segments: segments) }
+RSpec.describe ShippingContainer do
+  let(:crates) { [Crate.new(weight: 300), Crate.new(weight: 100)] }
+  let(:shipping_container) { ShippingContainer.new(destination: 'Hawaii', crates: crates, max_weight: 500, max_containers: 10) }
 
   describe 'attributes' do
-    it 'has a name' do
-      expect(road_trip.name).to eq 'Trip to SF'
+    it 'has a destination' do
+      expect(shipping_container.destination).to eq 'Hawaii'
     end
 
-    it 'allows updates to name' do
-      road_trip.name = 'Trip to NYC'
-      expect(road_trip.name).to eq 'Trip to NYC'
+    it 'has a max_weight' do
+      expect(shipping_container.max_weight).to eq 500
     end
 
-    it 'has segments' do
-      expect(road_trip.segments).to match_array segments
+    it 'allows updates to max_weight' do
+      shipping_container.max_weight = 800
+      expect(shipping_container.max_weight).to eq 800
+    end
+
+    it 'has a max_containers' do
+      expect(shipping_container.max_containers).to eq 10
+    end
+
+    it 'has crates' do
+      expect(shipping_container.crates).to match_array crates
     end
   end
 
-  describe 'working with segments', { pending: true } do
+  describe 'working with crates', { pending: true } do
     describe '#add_segment' do
       it 'adds object to segement collection' do
-        new_segment = Segment.new
-        road_trip.add_segment(new_segment)
-        expect(road_trip.segments).to include new_segment
+        new_segment = Crate.new
+        shipping_container.add_segment(new_segment)
+        expect(shipping_container.crates).to include new_segment
       end
     end
 
     describe 'distance calculations' do
       describe '#total_miles' do
-        it 'sums the miles of each segment' do
-          expect(road_trip.total_miles).to eq 1250
+        it 'sums the miles of each crate' do
+          expect(shipping_container.total_miles).to eq ShippingContainer
         end
       end
 
       describe '#average_miles_per_segment' do
-        it 'averages the miles of the trip over the segments' do
-          expect(road_trip.average_miles_per_segment).to eq 625
+        it 'averages the miles of the trip over the crates' do
+          expect(shipping_container.average_miles_per_segment).to eq 625
         end
       end
     end
