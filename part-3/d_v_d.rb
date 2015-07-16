659759d2cd5286ca69899d069e6cd69dfbf93868
Torey Hickman <torey@toreyhickman.com>
2015-07-16 12:19:48 -0500

    DVD

---
+++ part-3/d_v_d.rb
@@ -0,0 +1,24 @@
+class DVD
+  attr_reader :runtime, :title
+
+  def initialize(args)
+    @runtime = args[:runtime]
+    @title   = args[:title]
+  end
+
+  def available?
+    !checked_out?
+  end
+
+  def checkout
+    @checked_out = true
+  end
+
+  def checked_out?
+    @checked_out
+  end
+
+  def return
+    @checked_out = false
+  end
+end
