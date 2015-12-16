e4aaf7eea56c5c05c9523bfe782f4515579f025c
mrYakamoto <wehmenstein@gmail.com>
2015-12-16 11:12:21 -0800

    finish part 2, work with a collection

--- part-2/league.rb
+++ part-2/league.rb
@@ -7,4 +7,17 @@ class League
     @calculator = args.fetch(:calculator) { PointsCalculator.new }
   end
 
+  def standings
+    teams.sort_by! { |team| calculator.points_for_team(team)}
+    teams.reverse
+  end
+
+  def playoff_teams
+    standings.slice(0, teams.count / 2)
+  end
+
+  def position_for(name)
+    standings.index{ |team| team.name == name } + 1
+  end
+
 end
