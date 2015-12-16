ef757ddfe25d82520ff958326c0d6e41df5bf612
GregoryW <email": null>
2015-12-16 11:45:26 -0800

    finish part 3, eliminate code smells

--- part-3/c_d.rb
+++ part-3/c_d.rb
@@ -1,37 +1,11 @@
-class CD
-  attr_reader :runtime, :title, :artist
+require_relative 'media'
+
+class CD < Media
+  attr_reader :runtime, :artist
 
   def initialize(args ={})
-    @runtime = args[:runtime]
-    @title   = args[:title]
-    @artist  = args[:artist]
-  end
-
-  def available?
-    !checked_out?
-  end
-
-  def checkout
-    @checked_out = true
-  end
-
-  def checked_out?
-    @checked_out
-  end
-
-  def return
-    @checked_out = false
-  end
-
-  def damaged?
-    @damaged
-  end
-
-  def record_damage
-    @damaged = true
-  end
-
-  def repair
-    @damaged = false
+    super
+    @runtime = args.fetch(:runtime, 'unknown')
+    @artist  = args.fetch(:artist, 'unknown')
   end
 end
--- part-3/media_kiosk.rb
+++ part-3/media_kiosk.rb
@@ -1,36 +1,10 @@
-class MediaKiosk
-  attr_reader :name, :location, :items
+require_relative 'library'
+require_relative 'maintenance'
+
+class MediaKiosk < Library
+  include Maintenance
+
   def initialize(args = {})
-    @name = args[:name]
-    @location = args[:location]
-    @items = args[:items]
-  end
-
-  def damaged?
-    @damaged
-  end
-
-  def record_damage
-    @damaged = true
-  end
-
-  def repair
-    @damaged = false
-  end
-
-  def add_item(item)
-    items << item
-  end
-
-  def remove_item(item)
-    items.delete(item)
-  end
-
-  def available_items
-    items.select { |item| item.available? && !item.damaged? }
-  end
-
-  def unavailable_items
-    items - available_items
+    super
   end
 end
